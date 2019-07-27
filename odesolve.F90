ebc644e41513a2da10369860c0de1524ebdadf1c
Johannes Feist <johannes.feist@gmail.com>
2019-07-27 14:52:45 +0200

    remove some unnecessary stuff, and read in times to save solution vector from times.petsc

--- odesolve.F90
+++ odesolve.F90
@@ -8,12 +8,15 @@ PROGRAM main
 
   Mat            :: A
   PetscViewer    :: binv
-  Vec            :: u0, utmp
+  Vec            :: u0, v_times, v_times_seq
+  VecScatter     :: ctx
   TS             :: ts
-  PetscScalar    :: A0
+  PetscScalar, pointer :: times_scalar(:)
   PetscErrorCode :: ierr
-  integer        :: my_id
+  PetscInt       :: steps
+  integer        :: my_id, ii
   double precision :: tt, tstart
+  double precision, allocatable :: times(:)
 
   tstart = mpi_wtime()
 
@@ -30,9 +33,6 @@ PROGRAM main
   call MatLoad(A,binv,ierr);CHKERRA(ierr)
   call PetscViewerDestroy(binv,ierr);CHKERRA(ierr)
 
-  call MatCreateVecs(A,PETSC_NULL_VEC,utmp,ierr);CHKERRA(ierr)
-  call VecSetFromOptions(utmp,ierr);CHKERRA(ierr)
-
   call MatCreateVecs(A,PETSC_NULL_VEC,u0,ierr);CHKERRA(ierr)
   call VecSetFromOptions(u0,ierr);CHKERRA(ierr)
 
@@ -40,20 +40,22 @@ PROGRAM main
   call VecLoad(u0,binv,ierr);CHKERRA(ierr)
   call PetscViewerDestroy(binv,ierr);CHKERRA(ierr)
 
-  ! warmup CUDA (compile CUDA kernels?)
-  call MatMult(A,u0,utmp,ierr);CHKERRA(ierr)
-  ! WARNING: PETSc conjugates the SECOND vector argument, i.e., to get <x|y>, we have to call VecDot(y,x)
-  ! https://www.mcs.anl.gov/petsc/petsc-current/docs/manualpages/Vec/VecDot.html
-  call VecDot(utmp,u0,A0,ierr);CHKERRA(ierr)
+  call VecCreate(PETSC_COMM_WORLD,v_times,ierr);CHKERRA(ierr)
+  call PetscViewerBinaryOpen(PETSC_COMM_WORLD,'times.petsc',FILE_MODE_READ,binv,ierr);CHKERRA(ierr)
+  call VecLoad(v_times,binv,ierr);CHKERRA(ierr)
+  call PetscViewerDestroy(binv,ierr);CHKERRA(ierr)
 
-  tt = mpi_wtime()
-  call MatMult(A,u0,utmp,ierr);CHKERRA(ierr)
-  call VecDot(utmp,u0,A0,ierr);CHKERRA(ierr)
-  
-  if (my_id==0) then
-    write(6,*) 'time for MatMult:', mpi_wtime() - tt
-    write(6,*) '<u0|A|u0> =', A0
-  endif
+  call VecScatterCreateToAll(v_times,ctx,v_times_seq,ierr);CHKERRA(ierr)
+  call VecScatterBegin(ctx,v_times,v_times_seq,INSERT_VALUES,SCATTER_FORWARD,ierr);CHKERRA(ierr)
+  call VecScatterEnd(ctx,v_times,v_times_seq,INSERT_VALUES,SCATTER_FORWARD,ierr);CHKERRA(ierr)
+  call VecScatterDestroy(ctx,ierr);CHKERRA(ierr)
+  call VecGetArrayReadF90(v_times_seq,times_scalar,ierr);CHKERRA(ierr)
+  allocate(times(size(times_scalar)))
+  ! copy from complex to real
+  times = times_scalar
+  call VecRestoreArrayReadF90(v_times_seq,times_scalar,ierr);CHKERRA(ierr)
+  call VecDestroy(v_times_seq,ierr);CHKERRA(ierr)
+  call VecDestroy(v_times,ierr);CHKERRA(ierr)
 
   tt = mpi_wtime()
 
@@ -64,9 +66,24 @@ PROGRAM main
   call TSSetRHSJacobian(ts,A,A,TSComputeRHSJacobianConstant,PETSC_NULL_FUNCTION,ierr);CHKERRA(ierr)
   !call TSSetIFunction(ts,PETSC_NULL_VEC,TSComputeIFunctionLinear,PETSC_NULL_FUNCTION,ierr);CHKERRA(ierr)
   !call TSSetIJacobian(ts,A,A,TSComputeIJacobianConstant,PETSC_NULL_FUNCTION,ierr);CHKERRA(ierr)
+  call TSSetExactFinalTime(ts,TS_EXACTFINALTIME_MATCHSTEP,ierr);CHKERRA(ierr)
   call TSSetFromOptions(ts,ierr);CHKERRA(ierr)
 
-  call TSSolve(ts,u0,ierr);CHKERRA(ierr)
+  if (times(1)/=0.d0) then
+     SETERRA(PETSC_COMM_SELF,8,"first time in times.petsc must be 0!")
+  end if
+  call PetscViewerBinaryOpen(PETSC_COMM_WORLD,'usol.petsc',FILE_MODE_WRITE,binv,ierr);CHKERRA(ierr)
+  call VecView(u0,binv,ierr);CHKERRA(ierr)
+
+  do ii = 2, size(times)
+     call TSSetMaxTime(ts,times(ii),ierr);CHKERRA(ierr)
+     call TSSolve(ts,u0,ierr);CHKERRA(ierr)
+     call VecView(u0,binv,ierr);CHKERRA(ierr)
+     call TSGetStepNumber(ts,steps,ierr);CHKERRA(ierr)
+     if (my_id==0) write(6,'(A,G12.4,A,G12.4,A,I8,A)') 'propagated until t = ', times(ii), ' in ', mpi_wtime()-tt, ' seconds with ', steps, ' timesteps'
+  end do
+
+  call PetscViewerDestroy(binv,ierr);CHKERRA(ierr)
 
   if (my_id==0) then
     write(6,*) 'time for time propagation:', mpi_wtime() - tt
